3f27516a905a6b5f34f39bfd67a24a96548fbdf7
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-11-08 20:18:34 -0500

    Added style code to the style.css file and added questions for the quiz to the questions.js file

---
+++ JS/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        prompt: "What is the correct way to declare a variable in JavaScript?",
+        selections: ["var x", "variable x", "v x", "let x"],
+        answer: "let x"
+    },
+    {
+        prompt: "Which of the following is NOT a JavaScript data type?",
+        selections: ["number", "boolean", "string", "char"],
+        answer: "char"
+    },
+    {
+        prompt: "How do you comment out a single line in JavaScript?",
+        selections: ["// This is a comment", " /* This is a comment */", " ' This is a comment", "# This is a comment"],
+        answer: "// This is a comment"
+    },
+    {
+        prompt: "Which operator is used for equality comparison in JavaScript?",
+        selections: ["==", "=", "===", "!="],
+        answer: "==="
+    },
+    {
+        prompt: "Which JavaScript method is used to remove the last element from an array?",
+        selections: ["removeLast()", "pop()", "splice()", "slice()"],
+        answer: "pop()"
+    },
+    {
+        prompt: "Which loop is best suited for iterating through an array in JavaScript?",
+        selections: ["for...in loop", "while loop", "for loop", "forEach loop"],
+        answer: "for loop"
+    },
+    {
+        prompt: "What does the NaN value represent in JavaScript?",
+        selections: ["Not a Number", "Null and Nothing", "No Answer Needed", "Negative and Negative"],
+        answer: "Not a Number"
+    }
+];
